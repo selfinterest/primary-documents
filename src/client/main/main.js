1614045e3f7f82431f6d8972328167b1ad108ea2
terrence <parliamentaryterrence@gmail.com>
2015-11-07 15:17:53 -0500

    Cleaned up a bit

--- src/client/main/main.js
+++ src/client/main/main.js
@@ -13,31 +13,12 @@ var templateUrl = require("./main.tpl.jade");
 angular.module("PrimaryDocuments")
     .config(["$urlRouterProvider", "$stateProvider", "$locationProvider", function ($urlRouterProvider, $stateProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise("/");
         $stateProvider
             .state("main", {
                 url: "/",
                 templateUrl: templateUrl,
                 abstract: true
-                //abstract: true
             })
-           /* .state("main.top", {
-                url: "",
-                views: {
-                    "top": {
-                        template: "<div>Test</div>"
-                    }
-                }
-            })
-            .state("main.bottom", {
-                url: "",
-                views: {
-                    "bottom": {
-                        templateUrl: bottomTemplateUrl
-                    }
-                }
-
-
-            })
-            */
     }])
 ;
