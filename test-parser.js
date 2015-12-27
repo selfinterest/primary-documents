972d4740e5a445f083c9aa839e38d62842621374
terrence_watson <terrence.watson@mheducation.com>
2015-12-27 15:21:32 -0500

    Fixing

--- test-parser.js
+++ test-parser.js
@@ -1,6 +1,7 @@
 var strict = true
 	, fs = require("fs")
 	, parseString = require('xml2js').parseString
+	, util = require("util")
 	, options = {
 		explicitArray: false,
 		async: true
@@ -11,6 +12,6 @@ fs.readFile(__dirname + "/test-document.xml", function(err, data){
 	if(err) console.log(err);
 	parseString(data, options, function(err, results){
 		if(err) console.log(err);
-		console.log(results.primaryDocument);
+		console.log(util.inspect(results.primaryDocument, {depth: null}));
 	})
 })
